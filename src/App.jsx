c3de6673a07ec9f4fdd91405fe6d61b6a74a47bb
Serhii Kanzeba <ytre@MacBook-Air-Serhii.local>
2026-07-16 20:28:58 +0300

    Add tg API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Slot from "./components/Slot";
 
 function App() {
   const [coinBalance, setCoinBalance] = useState(1000);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const tg = window.Telegram?.WebApp;
+
+    if (!tg) return;
+
+    tg.ready();
+    setUser(tg.initDataUnsafe.user);
+  }, []);
 
   return (
     <>
-      <Header coinBalance={coinBalance} />
+      <Header coinBalance={coinBalance} user={user} />
       <Slot setCoinBalance={setCoinBalance} />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-export default function Header({ coinBalance }) {
+export default function Header({ coinBalance, user }) {
   return (
     <header className="flex justify-between p-3">
-      <h5>Hello user</h5>
+      <h5>{user?.username ?? "tester"}</h5>
       <p>{coinBalance + "$"}</p>
     </header>
   );
